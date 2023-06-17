a2e207bccd6819e2752237c53d7d08cd7ca27a40
Eugene Mandrikov <mr.evgen1002@yandex.ru>
2023-06-18 01:17:30 +0300

    first commit

---
+++ src/main/java/PageObject/LocatorsPersonalCabinetPage.java
@@ -0,0 +1,8 @@
+package PageObject;
+
+import org.openqa.selenium.By;
+
+public class LocatorsPersonalCabinetPage {
+    public static final By buttonSave = By.xpath("//*[@id=\"root\"]/div/main/div/div/div/div/button[2]");
+    public static final By buttonExit = By.xpath("/html/body/div/div/main/div/nav/ul/li[3]/button");
+}
